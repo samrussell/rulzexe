5ebdc2742b850d976e16eeeeced98ef20e5f021e
Sam Russell <sam.h.russell@gmail.com>
2018-04-23 20:53:49 +1200

    started adding specs to the decoder

--- spec/code_unpacker_spec.rb
+++ spec/code_unpacker_spec.rb
@@ -8,4 +8,25 @@ describe CodeUnpacker do
   let(:packed_data) do
     "\xFF\xFF\xBA\x05\x13.\x89\x165\x02\xB40\xCD!\x8B.\x02\xFF\xFF\x00\x8B\x1E,\x00\x8E\xDA\xA3\x90\x00\x8C\x06\x8E\x00\x89\x1E\xF0\x1F\x8A\xFC.\xA6\x00\xE8=\x01\xC4>\x88\xFE\xFF\xE4\xC7\x8B\xD8\xB9\xFF\x7F\xFC\xF2\xAE\xE3aC&8\xFF\xE1\x05u\xF6\x80\xCD\x80\xF7\xD9\x89\x0E\xE5\xB9\x01\xFF\x10\x00\xD3\xE3\x83\xC3\b\x83\xE3\xF8\xCB\x8C\xFE\x7F\xC3\xDA+\xEA\x8B".force_encoding("ASCII-8BIT")
   end
+
+  let(:file) { StringIO.new(packed_data) }
+  let(:unpacker) { CodeUnpacker.new(file) }
+
+  context "with 4 literals" do
+    let(:packed_data) { "\x2F\x00ABCD\x00\x00\x00" }
+    let(:unpacked_data) { "ABCD" }
+
+    it "unpacks correctly" do
+      expect(unpacker.unpack).to eq(unpacked_data)
+    end
+  end
+
+  context "with 18 literals" do
+    let(:packed_data) { "\xFF\xFFABCDEFGHIJKLMNO\x0B\x00PQR\x00\x00\x00" }
+    let(:unpacked_data) { "ABCDEFGHIJKLMNOPQR" }
+
+    it "unpacks correctly" do
+      expect(unpacker.unpack).to eq(unpacked_data)
+    end
+  end
 end
